edbde101e074aa82631031b46846ea93450ca202
FolkLab <FolkLab101@gmail.com>
2021-10-04 11:16:19 -0700

    Update FastDAC_PSD.py

--- FastDAC_PSD.py
+++ FastDAC_PSD.py
@@ -14,19 +14,19 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import time
+import dash_table
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
 
-fd = FD.FastDAC('COM13', baudrate=1750000, timeout=1, verbose=False)            # Optical cable
-#fd = FD.FastDAC('COM6', baudrate=57600, timeout=1, verbose=False)              # USB
+fd = FD.FastDAC('COM5', baudrate=1750000, timeout=1, verbose=False)            # Optical cable
+#fd = FD.FastDAC('COM6', baudrate=57600, timeout=1, verbose=False)             # USB
 FastDAC_ID = fd.IDN()
 
 def PSD(duration, channels=[0, ]):
     ts = time.time()
-    xfft = []
-    yfft = []
     xper = []
     yper = []
-    xwel = []
-    ywel = []
         
     c_time = list()
     for c in channels:
@@ -34,7 +34,7 @@ def PSD(duration, channels=[0, ]):
         if t_read not in c_time:
             c_time.append(t_read)
 
-    assert len(c_time) == 1
+    #assert len(c_time) == 1
 
     c_freq = 1/(c_time[0]*10**-6)  # Hz
     measure_freq = c_freq/len(channels)
@@ -50,7 +50,7 @@ def PSD(duration, channels=[0, ]):
     voltage_readings = []
 
     try:
-        while fd.ser.in_waiting > 24+15 or len(voltage_readings) <= num_bytes/2:
+        while fd.ser.in_waiting > 24 or len(voltage_readings) <= num_bytes/2:
             buffer = fd.ser.read(24)
             info = [buffer[i:i+2] for i in range(0, len(buffer), 2)]
             for two_b in info:
@@ -68,114 +68,97 @@ def PSD(duration, channels=[0, ]):
         channel_readings[k] = voltage_readings[k::len(channels)]
         channel_readings[k] = np.array(channel_readings[k])
 
-        y = fft(channel_readings[k])
-        yfft.append( [ 1/num_bytes * np.abs(y[0:num_bytes//2]) ] )
-        T = 1/measure_freq
-        xfft.append( [ fftfreq(num_bytes, T)[:num_bytes//2] ] )
-
         f, Pxx_den = signal.periodogram(channel_readings[k], measure_freq)
         xper.append([f])
         yper.append([Pxx_den])
 
-        f, Pxx_den = signal.welch(channel_readings[k], measure_freq) #, nperseg=24)
-        xwel.append([f])
-        ywel.append([Pxx_den])
+    return xper, yper, str(num_bytes), str(time.time() - ts)
 
-    return xfft, yfft, xper, yper, xwel, ywel, str(num_bytes), str(time.time() - ts)
+X = [[],[],[],[]]
+Y = [[],[],[],[]]
 
-X = []
-Y = []
+df = pd.DataFrame()
+data  = {'col1':'-', 'col2':'-', 'col3':'-', 'col4':'-'}
+df = df.append(data, ignore_index=True)
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
-    dcc.Graph(id="live-graph", animate=True),
-    dcc.Interval(id="graph-update", interval=1500, n_intervals=0),
-    dcc.Dropdown(
-        id='avg-dropdown',
-        options=[
-            {'label': 'Averaging', 'value': 'AVG'},
-            {'label': 'Normal', 'value': 'NRML'}
-        ],
-        value='AVG'
-    ),
 
-    html.Hr(),
-    dcc.Dropdown(
-        id='psd-dropdown',
-        options=[
-            {'label': 'Fast Fourier Transform (FFT)', 'value': 'FFT'},
-            {'label': 'Periodogram', 'value': 'PER'},
-            {'label': 'Welch', 'value': 'WEL'}
-        ],
-        value='FFT'
-    ),
-
-    html.Hr()
-])
-
-@app.callback(
-    Output('avg-dropdown', 'value'),
-    [Input('avg-dropdown', 'options')])
-
-@app.callback(
-    Output('psd-dropdown', 'value'),
-    [Input('psd-dropdown', 'options')])
+    html.Div([dcc.Graph(id="live-graph", animate=True)], style={'width': '150%', 'height':'150%'}),
+    dcc.Interval(id="graph-update", interval=2000, n_intervals=0),
+    html.Div([
+        dcc.Dropdown(
+            id='avg-dropdown', 
+            options=[
+                {'label': 'Averaging', 'value': 'AVG'},
+                {'label': 'Normal', 'value': 'NRML'}],
+            value='AVG')
+            ], 
+            style={'width': '15%', "margin-left": "30px"}),
+        
+    html.Div([
+        dash_table.DataTable(
+            id='table', 
+            columns=[{'name': 'FastDAC ID', 'id': 'col1'},
+                    {'name': 'Bytes/channel', 'id': 'col2'},
+                    {'name': 'Run time', 'id': 'col3'},
+                    {'name': 'Channels', 'id': 'col4'}],
+            data=df.to_dict('records'),
+            #fill_width=False,
+            style_cell={'textAlign': 'left', "margin-left": "30px"},
+            style_header={
+                'backgroundColor': 'rgb(230, 230, 230)',
+                'fontWeight': 'bold'})
+                ], 
+            style={'width': '40%', 'display': 'inline-block', "margin-left": "30px"})
+    ])
 
 @app.callback(
-    Output('live-graph', 'figure'),
-    [Input('graph-update', 'n_intervals'),
-    Input('psd-dropdown', 'value'),
-    Input('avg-dropdown', 'value')])
+    Output(component_id='avg-dropdown', component_property='value'),
+    [Input(component_id='avg-dropdown', component_property='options')])
 
-def update_graph(input_data, selected_psd, selected_avg):
-    channel_arr = [0,]
+@app.callback(
+    Output(component_id='live-graph', component_property='figure'),
+    Output(component_id='table', component_property='data'),
+    [Input(component_id='graph-update', component_property='n_intervals'),
+    Input(component_id='avg-dropdown', component_property='value')])
+
+def update_graph(input_data, selected_avg):
+    channel_arr = [1,2]
     psd = PSD(1, channels=channel_arr)
     fig = make_subplots(rows=[1,2,2,2][len(channel_arr)-1], cols=[1,1,2,2][len(channel_arr)-1])
-    fig.update_layout(height=600, width=1000, title_text="PSD", legend_title="Legend")
+    fig.update_layout(height=600, width=1000, title_text="FastDAC PSD", legend_title="Channels")
     fig.update_yaxes(type='log', title_text='Potential [mV]')
     fig.update_xaxes(title_text='Frequency [Hz]')
-    fig.add_annotation(text="num_bytes="+psd[6]+", Runtime="+psd[7]+'s'+", FastDAC ID="+FastDAC_ID+", channels="+str(channel_arr),
-                                xref="paper", yref="paper",x=0, y=1.05, showarrow=False)
-
-    if selected_psd=='FFT':
-        x_i = 0
-        y_i = 1
-    elif selected_psd=='PER':
-        x_i = 2
-        y_i = 3
-    elif selected_psd=='WEL':
-        x_i = 4
-        y_i = 5
 
     for k in range(0, len(channel_arr)):
-        X.append(psd[x_i][k][0])
-        Y.append(psd[y_i][k][0])
+        X[k].append(psd[0][k][0])
+        Y[k].append(psd[1][k][0])
         
-        if len(X)<5 and selected_avg=='AVG':
-            xnew=np.mean(X, axis=0) 
-            ynew=np.mean(Y, axis=0)
+        if len(X[k])<5 and selected_avg=='AVG':
+            xnew=np.mean(X[k], axis=0)
+            ynew=np.mean(Y[k], axis=0)
             
-
         elif selected_avg=='AVG':
-            xnew=np.mean(X[-6:-1], axis=0)
-            ynew=np.mean(Y[-6:-1], axis=0)
+            xnew=np.mean(X[k][-6:-1], axis=0)
+            ynew=np.mean(Y[k][-6:-1], axis=0)
          
         elif selected_avg=='NRML':
-            xnew=psd[x_i][k][0]
-            ynew=psd[y_i][k][0]
+            xnew=psd[0][k][0]
+            ynew=psd[1][k][0]
                 
         fig.add_trace(
             go.Scatter(
                 x=xnew, 
                 y=ynew, 
-                name='Ch.'+ str(channel_arr[k])), 
+                name=str(channel_arr[k])), 
                 row=[1,2,1,2][k], 
-                col=[1,1,2,2][k]
-                )
+                col=[1,1,2,2][k])
 
-    return fig
-
+    df = pd.DataFrame()
+    data={'col1':FastDAC_ID, 'col2':psd[2], 'col3':str(round(float(psd[3]), 2)), 'col4':str(channel_arr)}
+    df = df.append(data, ignore_index=True)
+    return fig, df.to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
